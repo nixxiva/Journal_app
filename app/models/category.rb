a2347f16c62899c9d96729f0396feecde8387f54
Nixxiva <sarahbustinera24@gmail.com>
2025-07-05 21:41:27 +0800

    Adding validation for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :tasks
+
+  validates :title, presence: true
+  validates :body, presence: true, length: {minimum: 5, maximum: 100}
 end
