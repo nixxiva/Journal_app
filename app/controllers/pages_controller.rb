395776729f927a72f687f5b9ccb540a9f171f04f
Nixxiva <sarahbustinera24@gmail.com>
2025-07-08 00:23:42 +0800

    Added show task for today

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,7 @@ class PagesController < ApplicationController
     @user = current_user
     @categories = @user.categories
     @tasks = @user.tasks
+    @all_category = Category.all
+    @tasks_today = Task.where(due_date: Date.today)
   end
 end
