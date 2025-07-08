0b3388c02e9caf5d9f08f8ceed4b1907bf71303e
Nixxiva <sarahbustinera24@gmail.com>
2025-07-09 01:32:30 +0800

    Added testing

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without title" do
+    category = Category.new(body: "Test body")
+    assert_not category.save
+  end
+
+  test "should not save without body" do
+    category = Category.new(title: "Test body")
+    assert_not category.save
+  end
+
+  test "should not save with < 5 chars " do
+    category = Category.new(title: "Test title", body: "123")
+    assert_not category.save
+  end
+
+  test "should save article with valid params" do
+    category = Category.new(title: "Test title", body: "Test body", user: users(:one))
+    assert category.save
+  end
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without task name" do
+    task = Task.new(definition: "Test definition", due_date: "07-01-2025")
+    assert_not task.save
+  end
+
+  test "should not save without definition" do
+    task = Task.new(task_name: "Test task name", due_date: "07-01-2025")
+    assert_not task.save
+  end
+
+  test "should not save without due date" do
+    task = Task.new(definition: "Test definition", task_name: "Test task name")
+    assert_not task.save
+  end
+
+  test "should not save with < 5 chars" do
+    task = Task.new(task_name: "Test task name", definition: "123", due_date: "07-01-2025")
+    assert_not task.save
+  end
+
+  test "should save task with valid params" do
+    task = Task.new(task_name: "Test task name", definition: "Test definition", due_date: "07-09-2025", category: categories(:one))
+    assert task.save
+  end
 end
+
