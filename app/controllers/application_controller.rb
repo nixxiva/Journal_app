e2e2dd653c807b3ce3adfb8e890245fcc5e7d091
Nixxiva <sarahbustinera24@gmail.com>
2025-07-10 00:54:16 +0800

    Fixed show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,12 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
-
+  
   def after_sign_in_path_for(resource)
     dashboard_path
+  end
+
+  def after_sign_up_path_for(resource)
+    new_user_session_path
   end
 end
